803d3eb5d2394e572030817c267053128231a120
Janet Mancha <janet.mancha@gmail.com>
2016-02-11 23:47:43 +0100

    Initial Commit

---
+++ Velocimetro Automovil Digital/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Velocimetro Automovil Digital
+//
+//  Created by janet on 11/2/16.
+//  Copyright © 2016 janet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
